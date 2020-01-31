78216a8bb2a4d66a54bf74210639ed413b689bdd
carlosweb <kaduinfo@gmail.com>
2020-01-31 20:55:17 -0300

    angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-list';
+  public titulo: string = "My first Angular App";
+
+
 }
